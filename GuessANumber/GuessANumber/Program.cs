cb8d330ae3594e61ed5acecea82ea6d93b1bdc81
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2022-09-25 23:03:22 +0300

    Add files via upload

---
+++ GuessANumber/GuessANumber/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GuessANumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int randomNumber = random.Next(1, 100);
+
+            Console.WriteLine("Let's play a game?");
+            Console.WriteLine("The game is \"Guess A Number\"");
+            Console.WriteLine("It is easy, I have a number, You try to guess it?");
+            Console.WriteLine("Let's play?");
+            Console.WriteLine();
+
+            while (true)
+            {
+                Console.Write("Guess a number (1-100): ");
+
+                int n = int.Parse(Console.ReadLine());
+                string message = "";
+
+                if (n == randomNumber)
+                    break;
+
+                if (((randomNumber - n) <=5) && ((randomNumber - n) > 0))
+                    message = "Low";
+
+                if (((randomNumber - n) > 5))
+                    message = "Too Low";
+
+                if (((randomNumber - n) >= -5) && ((randomNumber - n) < 0))
+                    message = "High";
+
+                if (((randomNumber - n) < -5))
+                    message = "Too HIgh";
+
+                Console.WriteLine($"{message}");
+            }
+
+            Console.WriteLine("You guessed it!");
+        }
+    }
+}
